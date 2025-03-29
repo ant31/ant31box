f3884beb86c2bd7ce8a5355d870ec99ed2d96cfd
ant31 <2t.antoine@gmail.com>
2025-03-29 22:54:04 +0100

    utils

--- ant31box/utilsd.py
+++ ant31box/utilsd.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python3
-from typing import Mapping
+import importlib
+from typing import Mapping, Any
+
+
+
+# from uvicorn.importer (BSD 3-Clause https://github.com/encode/uvicorn/blob/master/LICENSE.md)
+class ImportFromStringError(Exception):
+    pass
+
+# From uvicorn.importer (BSD 3-Clause https://github.com/encode/uvicorn/blob/master/LICENSE.md)
+def import_from_string(import_str: Any) -> Any:
+    if not isinstance(import_str, str):
+        return import_str
+
+    module_str, _, attrs_str = import_str.partition(":")
+    if not module_str or not attrs_str:
+        message = 'Import string "{import_str}" must be in format "<module>:<attribute>".'
+        raise ImportFromStringError(message.format(import_str=import_str))
+
+    try:
+        module = importlib.import_module(module_str)
+    except ModuleNotFoundError as exc:
+        if exc.name != module_str:
+            raise exc from None
+        message = 'Could not import module "{module_str}".'
+        raise ImportFromStringError(message.format(module_str=module_str))
+
+    instance = module
+    try:
+        for attr_str in attrs_str.split("."):
+            instance = getattr(instance, attr_str)
+    except AttributeError:
+        message = 'Attribute "{attrs_str}" not found in module "{module_str}".'
+        raise ImportFromStringError(message.format(attrs_str=attrs_str, module_str=module_str))
+
+    return instance
 
 
 def deepmerge(dct, merge_dct):
